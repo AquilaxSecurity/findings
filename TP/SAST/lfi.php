bf7de5291bbf46878a3ca35399b8de98069fe69f
AquilaxSecurity <aditya@aquilax.io>
2025-01-09 18:41:28 +0530

    Update lfi.php

--- TP/SAST/lfi.php
+++ TP/SAST/lfi.php
@@ -1,6 +1,6 @@
 <?php
-
+#Retrieves a value from the file in the URL and assigns it to the $data variable. Injecting malicious input in the file paramater, this can lead to LFI.
 $data = $_GET['file'];
-
+ # It loads the file specified in the file parameter.
 include($data);
 ?>
